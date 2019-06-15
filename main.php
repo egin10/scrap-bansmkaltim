867b831cc00831bd29dd326d6b981a720220a84a
egin10 <egin066@gmail.com>
2019-06-15 07:01:10 +0700

    fix

--- main.php
+++ main.php
@@ -17,28 +17,43 @@ $ch_url = curl_init($base_url);
 curl_setopt_array($ch_url, [CURLOPT_RETURNTRANSFER => true]);
 $get_url = curl_exec($ch_url);
 
-$dataSch = $getData->getDataSchool($get_url);
+$dataSch = [];
+$dataSch[] = [
+	"no" => "NO",
+	"schName" => "NAMA SEKOLAH",
+	"schLevel" => "JENJANG",
+	"schDist" => "KABUPATEN/KOTA",
+	"schSubDist" => "KECAMATAN",
+	"schVil" => "DESA/KELURAHAN",
+	"schAddr" => "ALAMAT SEKOLAH",
+	"schPhone" => "NO HP",
+	"schStats" => "STATUS"
+];
+foreach($getData->getDataSchool($get_url) as $v)
+{
+	$dataSch[] = $v;
+};
 
 print_r($dataSch);
 
 curl_close($ch_url);
 
 //Write xlsx
-// ini_set('display_errors', 0);
-// ini_set('log_errors', 1);
-// error_reporting(E_ALL & ~E_NOTICE);
-// $filename = implode("_", explode(" ", $nameProv))."_".date('d-m-Y').".xlsx";
-// $writer = new XLSXWriter();
-// $writer->setAuthor('egin10'); 
-// foreach($arrSekolah as $row){
-// 	if($row != NULL) $writer->writeSheetRow('Sheet1', $row);
-// }
-// $writer->writeToFile($filename);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+$filename = "Data_SM_bansmkaltimId_".date('d-m-Y').".xlsx";
+$writer = new XLSXWriter();
+$writer->setAuthor('egin10'); 
+foreach($dataSch as $row){
+	if($row != NULL) $writer->writeSheetRow('Sheet1', $row);
+}
+$writer->writeToFile($filename);
 
-// $mv = rename($filename, __DIR__."/FILES/".$filename);
-// if($mv) {
-// 	echo "File created!\n";
-// 	echo "DONE!\n";
-// }
+$mv = rename($filename, __DIR__."/FILES/".$filename);
+if($mv) {
+	echo "File created!\n";
+	echo "DONE!\n";
+}
 
 unset($getData);
--- src/func.php
+++ src/func.php
@@ -18,10 +18,37 @@ class GetData
         return substr($teks, $ini, $panjang);
     }
 
-    public function getDataSchool(String $url): String
+    public function getDataSchool(String $url): Array
     {
-        $dataSch = $this->getStringBetween($url, "</thead>", '<script type="text/javascript">');
+        $data = [];
+        $dataSch = trim($this->getStringBetween($url, "</thead>", '<script type="text/javascript">'));
+        $arrDataSch = explode('<tr onclick="javascript:pilih(this);">', $dataSch); //iterate from 1 not 0
+        for($i=1; $i < count($arrDataSch); $i++)
+        {
+            $explDataSch = explode("<td", $arrDataSch[$i]);
+            $no = trim($this->getStringBetween($explDataSch[1],">","</td>"));
+            $schName = trim($this->getStringBetween($explDataSch[2],">","</td>"));
+            $schLevel = trim($this->getStringBetween($explDataSch[3],">","</td>"));
+            $schDist = trim($this->getStringBetween($explDataSch[4],">","</td>"));
+            $schSubDist = trim($this->getStringBetween($explDataSch[5],">","</td>"));
+            $schVil = trim($this->getStringBetween($explDataSch[6],">","</td>"));
+            $schAddr = trim($this->getStringBetween($explDataSch[7],">","</td>"));
+            $schPhone = trim($this->getStringBetween($explDataSch[8],">","</td>"));
+            $schStats = trim($this->getStringBetween($explDataSch[9],">","</td>"));
+            
+            $data[] = [
+                "no" => $no,
+                "schName" => $schName,
+                "schLevel" => $schLevel,
+                "schDist" => $schDist,
+                "schSubDist" => $schSubDist,
+                "schVil" => $schVil,
+                "schAddr" => $schAddr,
+                "schPhone" => $schPhone,
+                "schStats" => $schStats
+            ];
+        }
 
-        return $dataSch;
+        return $data;
     }
 }
